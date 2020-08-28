009e454c826a7fd64ddff7d15be960f1d6cb1d23
mischkiey <mischkieycf@gmail.com>
2020-08-28 14:04:36 -0400

    Completed queue class implementation

--- modules/pets/pets.service.js
+++ modules/pets/pets.service.js
@@ -16,10 +16,18 @@ store.dogs.forEach(dog => pets.dogs.enqueue(dog));
 
 module.exports = {
   get() {
-    // Return the pets next in line to be adopted.
+    return {
+        cats: pets.cats.all(),
+        dogs: pets.dogs.all(),
+    }
   },
 
   dequeue(type) {
     // Remove a pet from the queue.
-  }
+    if(type === 'cats') {
+        return pets.cats.dequeue();
+    }
+
+    return pets.dogs.dequeue();
+  },
 };
--- modules/queue/Queue.js
+++ modules/queue/Queue.js
@@ -1,23 +1,73 @@
-class Queue {
-  constructor() {
-    // Set initial data.
-  }
-
-  enqueue(data) {
-    // Add some data to the queue.
-  }
-
-  dequeue() {
-    // Remove some data from the queue.
-  }
-
-  show() {
-    // Return the next item in the queue.
-  }
-
-  all() {
-    // Return all items in the queue.
-  }
+class _Node {
+    constructor(data, next) {
+        this.data = data;
+        this.next = next;
+    };
 };
 
-module.exports = Queue
+class Queue {
+    constructor() {
+        this.first = null;
+        this.last = null;
+    };
+
+    enqueue(data) {
+        // Add some data to the queue.
+        const newNode = new _Node(data, null);
+
+        if(this.first === null) {
+            this.first = newNode;
+            // this.last = this.first;
+        };
+
+        if(this.last) {
+            this.last.next = newNode;
+        };
+
+        this.last = newNode;
+    };
+
+    dequeue() {
+        // Remove some data from the queue.
+        if(this.first === null) {
+            // throw new Error('No data in queue');
+            return;
+        };
+        const tempNode = this.first;
+        this.first = tempNode.next;
+
+        if(tempNode === this.last) {
+            this.last = null;
+        };
+
+        return tempNode.value;
+    };
+
+    show() {
+        // Return the next item in the queue.
+        if(this.first === null) {
+            throw new Error('No item in the queue');
+        };
+
+        return this.first;
+    };
+
+    all() {
+        // Return all items in the queue.
+        if(this.first === null) {
+            throw new Error('No items in the queue');
+        };
+
+        let currentNode = this.first;
+        let queue = [];
+
+        while(currentNode.next !== null) {
+            queue.push(currentNode);
+            currentNode = currentNode.next;
+        };
+
+        return queue;
+    };
+};
+
+module.exports = Queue;
